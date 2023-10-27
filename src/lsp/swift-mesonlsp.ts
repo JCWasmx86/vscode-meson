15351ab6002e6d7e879492b0d4bd6a5490864f76
JCWasmx86 <JCWasmx86@t-online.de>
2023-10-27 17:12:58 +0000

    Swift-MesonLSP: Bump to v3.0.20

--- src/lsp/swift-mesonlsp.ts
+++ src/lsp/swift-mesonlsp.ts
@@ -8,21 +8,21 @@ export class SwiftMesonLspLanguageClient extends LanguageServerClient {
   private static artifacts: { [key: string]: { name: string; hash: string } } = {
     "win32-x64": {
       name: "Swift-MesonLSP-win64.zip",
-      hash: "5a43030f6bcd98bc1c4fdced827f0185af08fe7397cc08ecd29878b8fff3b18d",
+      hash: "559c040126969c9ab9c681d8e97efc06a46ed5f57064186b3e1b1c2cf1ae224c",
     },
     "darwin-x64": {
       name: "Swift-MesonLSP-macos12.zip",
-      hash: "024457775bbaa55ce1a0c1ba65a8f92c4c7879ee42c7d7259f131bbb02fb3fa2",
+      hash: "06c684841234c938f0c8a6e638d4b10610c44b2cd50f3a82ca6255ac36b68543",
     },
     "linux-x64": {
       name: "Swift-MesonLSP.zip",
-      hash: "f0bd8294a91feecdaa6aeedb20d390f445d74bfba37c274de981878cc26b2a2b",
+      hash: "a9ba971d061be3610fc92bdb1daa771fb36469d024e1ee81cad6dd3b0623d157",
     },
   };
 
   static override repoURL: string = "https://github.com/JCWasmx86/Swift-MesonLSP";
   static override setupURL: string = "https://github.com/JCWasmx86/Swift-MesonLSP/tree/main/Docs";
-  static override version: string = "3.0.6";
+  static override version: string = "3.0.20";
 
   get runExe(): Executable {
     return {
